9d6a732589c52e3bd578820aebb7ad4842fdf08f
SivertsevMax <sivertsev1995@icloud.com>
2023-02-01 23:03:46 +0300

    my app

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  DiceRoll
-//
-//  Created by Максим Сиверцев on 01.02.2023.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    @IBOutlet weak var rollButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let delay = DispatchTime.now() + 2
+        DispatchQueue.main.asyncAfter(deadline: delay){
+            
+        }
+        diceImageViewOne.image = UIImage(named: "2")
+        diceImageViewTwo.image = UIImage(named: "3")
+        rollButton.layer.cornerRadius = 5
+        diceImageViewOne.layer.cornerRadius = 5
+        diceImageViewTwo.layer.cornerRadius = 5
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let imageDice = ["1", "2", "3", "4", "5", "6"]
+        diceImageViewOne.image = UIImage(named: imageDice.randomElement()!)
+        diceImageViewTwo.image = UIImage(named: imageDice.randomElement()!)
+    }
+    
 }
 
